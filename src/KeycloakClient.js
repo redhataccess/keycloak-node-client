b8d1961db74c3416291a50df187ac3f1949c9534
Jared Sprague <jared@caramelcode.com>
2023-02-07 12:52:05 -0500

    Completed initial implementation

--- src/KeycloakClient.js
+++ src/KeycloakClient.js
@@ -1,21 +1,78 @@
+import fetch from "node-fetch";
+
 class KeycloakClient {
-    constructor(config) {
-        this.config = config;
-        this.jwt = {};
+  constructor({
+    url,
+    realm,
+    grantType = "client_credentials",
+    clientId,
+    clientSecret,
+    scopes = ["openid"],
+    expiryGrace = 30,
+  }) {
+    this._jwt = {};
+    this.config = {
+      url,
+      realm,
+      grantType,
+      clientId,
+      clientSecret,
+      scopes,
+      expiryGrace,
+    };
+
+    if (!this.config.url.endsWith("/")) {
+      this.config.url += "/";
+    }
+  }
+
+  async freshToken() {
+    // Get the JWT auth token based on the config
+    const tokenEndpointUrl =
+      this.config.url + "realms/" + this.config.realm + "/protocol/openid-connect/token";
+
+    const formData = new URLSearchParams();
+    formData.append("grant_type", this.config.grantType);
+    formData.append("client_id", this.config.clientId);
+    formData.append("client_secret", this.config.clientSecret);
+    formData.append("scope", this.config.scopes.join(" "));
+
+    const response = await fetch(tokenEndpointUrl, {
+      method: "POST",
+      body: formData.toString(),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+
+    if (response.status !== 200) {
+      const reponseText = await response.text();
+      throw Error("Cannot get SSO Access Token. Server response=" + reponseText);
     }
 
-    getJWT() {
-        // Get the JWT based on the config
+    this._jwt = await response.json();
+    this._jwt.created = Date.now();
 
+    return this._jwt.access_token;
+  }
 
-        this.jwt = {
-            token: "foo",
-            expires_in: 1000,
-            scope: String,
-        }
-
-        return this.jwt;
+  async refreshToken() {
+    if (!this._jwt || !this._jwt.access_token || this.isJWTExpiringSoon()) {
+      // Get a new token
+      await this.freshToken();
     }
+
+    return this._jwt.access_token;
+  }
+
+  isJWTExpiringSoon() {
+    const elapsedSeconds = (Date.now() - this._jwt.created) / 1000;
+
+    if (elapsedSeconds + this.config.expiryGrace >= this._jwt.expires_in) {
+      // This token is expiring soon
+      return true;
+    }
+
+    return false;
+  }
 }
 
 export default KeycloakClient;
--- test/index.js
+++ test/index.js
@@ -1,7 +1,54 @@
-import KeycloakClient from "../src/KeycloakClient";
+import KeycloakClient from "../src/KeycloakClient.js";
 import assert from "node:assert";
+import test from "node:test";
 
-let client = new KeycloakClient({});
-const jwt = client.getJWT();
+function createClient() {
+  return new KeycloakClient({
+    url: process.env.URL,
+    realm: process.env.REALM,
+    scopes: [process.env.SCOPE],
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+}
 
-assert.equal(jwt.token, "foo");
+test("Test getting fresh token", async () => {
+  let client = createClient();
+
+  const token = await client.freshToken();
+
+  assert(token.length > 0);
+});
+
+test("Test refreshing unexpired token", async () => {
+  let client = createClient();
+
+  const token = await client.freshToken();
+
+  assert(token.length > 0);
+
+  setTimeout(async () => {
+    const newToken = await client.refreshToken();
+
+    // only 5 seconds has past so the token should not have expired yet
+    assert.strictEqual(token, newToken);
+  }, 5000);
+});
+
+test("Test refreshing expiring token", async () => {
+  let client = createClient();
+
+  const token = await client.freshToken();
+
+  assert(token.length > 0);
+
+  // Hack the expires_in property to make is really short
+  client._jwt.expires_in = 30;
+
+  setTimeout(async () => {
+    const newToken = await client.refreshToken();
+
+    // Token expires soon so it should be refreshed with new token
+    assert.notStrictEqual(token, newToken);
+  }, 5000);
+});
